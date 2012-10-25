05c2a1308e7fdc53ca220b14f44cee51ad453956
devbhave <devendrabhave@gmail.com>
2012-10-26 02:04:01 +0530

    Added servo support. Calibration done.

--- FireBird/prjParams.h
+++ FireBird/prjParams.h
@@ -12,5 +12,17 @@
  #endif /* F_CPU */
 
  #define MOTOR_SAFETY_DELAY    1000
+ 
+ /* Servo motor parameters:
+    SERVOx_MIN: Value of OCR1xL register corresponding to zero degrees
+    SERVOx_MAX: Value of OCR1xL register corresponding to 180 degrees
+  */
+ #define SERVO1_MIN 	40
+ #define SERVO1_MAX 	135
+ #define SERVO2_MIN 	35
+ #define SERVO2_MAX 	135
+ #define SERVO3_MIN 	35
+ #define SERVO3_MAX 	130
+ 
  #endif /* PRJ_PARAMS_H */
  
